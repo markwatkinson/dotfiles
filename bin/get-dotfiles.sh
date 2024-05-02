9c9fcd6afccd47cde3d7bf5b4c90f67fb6a52dbf
Mark Watkinson <markwatkinson@gmail.com>
2024-05-02 20:01:34 +0100

    Clean up get-dotfiles following review.
      - Change #! to more portable env bash
      - Change global git setting to local
      - Formatting

--- bin/get-dotfiles.sh
+++ bin/get-dotfiles.sh
@@ -1,19 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 git clone --bare git@github.com:markwatkinson/dotfiles.git $HOME/.dotfiles
+
 function dotfiles {
    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
+
 mkdir -p .dotfiles-backup
 dotfiles checkout
+
 if [ $? = 0 ]; then
-  echo "Checked out dotfiles";
+  echo "Checked out dotfiles"
   else
-    echo "Couldn't check out dotfiles due to existing files. Backing up existing files. to .dotfiles-backup";
+    echo "Couldn't check out dotfiles due to existing files. Backing up existing files to .dotfiles-backup"
+
     dotfiles checkout 2>&1 | grep -E "^\s+\S+" | awk {'print $1'} | xargs -d $'\n' sh -c 'for arg do echo "Backing up $arg"; mkdir -p .dotfiles-backup/"$arg"; mv "$arg" ".dotfiles-backup/$arg"; done' _
+
     echo 'Backed up'
 fi;
 
 dotfiles checkout
-dotfiles config status.showUntrackedFiles no
+dotfiles config --local status.showUntrackedFiles no
+
 echo 'Done'
