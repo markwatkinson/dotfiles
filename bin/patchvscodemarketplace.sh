f95f4c2c4b7b8e1c6141ea53f88ffd5e4097ab6a
Mark Watkinson <markwatkinson@gmail.com>
2024-05-26 17:22:53 +0100

    Add check for .rmpnew file

--- bin/patchvscodemarketplace.sh
+++ bin/patchvscodemarketplace.sh
@@ -4,23 +4,35 @@
 # extension marketplace. Use -R to revert.
 
 declare -a possibilities=(
-	'/usr/share/codium/resources/app/product.json'
-	'/usr/share/vscodium/resources/app/product.json'
+  '/usr/share/codium/resources/app/product.json'
+  '/usr/share/vscodium/resources/app/product.json'
 )
+
 productpath=''
 for p in "${possibilities[@]}"
 do
-	if [ -f "$p" ]; then
-		productpath=$p
-        fi
+  if [ -f "$p" ]; then
+    productpath=$p
+    fi
 done
 
 if [[ "$productpath" = "" ]]; then
-	echo "Could not find a valid product.json path"	
-	exit 1
+  echo "Could not find a valid product.json path"
+  exit 1
 fi
 
-
+rpmnewpath="$productpath.rpmnew"
+echo "Looking for .rpmnew file at $rpmnewpath"
+if [ -f "$rpmnewpath" ] ; then
+  if [ "$rpmnewpath" -nt "$productpath" ] ; then
+    echo ".rpmnew file exists at $rpmnewpath and is newer. Restoring to this first."
+    mv "$productpath" "$productpath.old"
+    cp "$rpmnewpath" "$productpath"
+  else
+      echo ".rpmnew file exists but is older than .product.json. Ignoring"
+    echo
+  fi
+fi
 
 if [ "${1}" = "-R" ]; then
   sed -i -e 's/^[[:blank:]]*"serviceUrl":.*/    "serviceUrl": "https:\/\/open-vsx.org\/vscode\/gallery",/' \
@@ -28,12 +40,14 @@ if [ "${1}" = "-R" ]; then
     -e 's/^[[:blank:]]*"itemUrl":.*/    "itemUrl": "https:\/\/open-vsx.org\/vscode\/item"/' \
     -e '/^[[:blank:]]*"linkProtectionTrustedDomains/d' \
     -e '/^[[:blank:]]*"documentationUrl/i\  "linkProtectionTrustedDomains": ["https://open-vsx.org"],' \
-    /usr/share/codium/resources/app/product.json
+    $productpath
+    echo "Patched"
 else
   sed -i -e 's/^[[:blank:]]*"serviceUrl":.*/    "serviceUrl": "https:\/\/marketplace.visualstudio.com\/_apis\/public\/gallery",/' \
     -e '/^[[:blank:]]*"cacheUrl/d' \
     -e '/^[[:blank:]]*"serviceUrl/a\    "cacheUrl": "https:\/\/vscode.blob.core.windows.net\/gallery\/index",' \
     -e 's/^[[:blank:]]*"itemUrl":.*/    "itemUrl": "https:\/\/marketplace.visualstudio.com\/items"/' \
     -e '/^[[:blank:]]*"linkProtectionTrustedDomains/d' \
-    /usr/share/codium/resources/app/product.json
+    $productpath
+    echo "Reverted"
 fi
