804ef1a7482b2c6413e83748b381361fb57d1b4b
Mark Watkinson <markwatkinson@gmail.com>
2024-08-18 16:18:21 +0100

    Add Chrome Geforce now script (enables hardware video decoding)

---
+++ bin/chrome-gfn.sh
@@ -0,0 +1,5 @@
+google-chrome \
+	--use-gl=angle \
+	--use-angle=vulkan \
+	--enable-accelerated-video-decode \
+	--enable-features=Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport,UseMultiPlaneFormatForHardwareVideo \
