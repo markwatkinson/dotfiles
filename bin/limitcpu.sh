38c13730423256ec8e32a86fff02b8ca81cbc467
Mark Watkinson <markwatkinson@gmail.com>
2024-10-21 19:32:24 +0100

    limitcpu.sh: Add better usage docs

--- bin/limitcpu.sh
+++ bin/limitcpu.sh
@@ -6,30 +6,42 @@ fields=($data)
 limited=${fields[0]}
 unlimited=${fields[1]}
 
-
 target=""
 
-if [ "${1}" == "" ]; then
+function print_usage() {
+    echo "Usage: $(basename $BASH_SOURCE) [--limit [freq]|--unlimit]"
+    echo ""
+    echo "      --limit [freq]     Sets the limit. freq is optional and"
+    echo "                         if not given will default to the lowest"
+    echo "                         freq supported by the CPU."
+    echo "                         freq can be given in any format accepted"
+    echo "                         by cpupower, e.g. 1000Mhz, 1Ghz"
+    echo ""
+    echo "      --unlimit          Removes any limit"
+}
+
+if [ "${1}" == "" ] || [ "${1}" == "--help" ]; then
     cpupower frequency-info -p
     echo ""
-    echo "Usage: ${0} [--limit|--unlimit]"
+    print_usage
     exit 0
 elif [ "${1}" = "--unlimit" ]; then
-    #sudo systemctl start power-profiles-daemon.service
-    #systemctl start --user power-profile.timer
     target="$unlimited"
-
-elif [ "${1}" = "--limit" ]; then
-    #sudo systemctl stop power-profiles-daemon.service
-    #systemctl stop --user power-profile.timer
+elif [ "${1}" = "--limit" ] && [ "${2}" == "" ]; then
     target="$limited"
+elif [ "${1}" = "--limit" ]; then
+    target="${2}"
 else
-    echo "Usage: ${0} [--limit|--unlimit]"
+    print_usage
     exit 1
 fi
 
 echo "Got limits as: low: $limited, high: $unlimited"
 
 sudo cpupower frequency-set --max $target
+if [ $? -ne 0 ]; then
+    echo "Failed to set frequency to $target"
+    exit 1
+fi
 echo "Set limit to $target"
-
+cpupower frequency-info -f -m
